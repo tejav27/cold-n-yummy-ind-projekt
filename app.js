59c53156810c895ba0d092b2434f4656c032ee7b
Tejaswi Vutukuri <tejaswi.vutukuri@iths.com>
2022-03-31 23:43:26 +0200

    Add git ignore and basic html page

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
+const express = require('express')
+
 const sequelize = require("./database/connection");
 
-const {User, Flavor} = require('./models/index')
+const {User, Flavor} = require('./models/index');
 
-sequelize
-.sync({force:true})
-.then(result => {
-    return User.create({username:"abcd",email:"acdd@jchd.com"})
+const app = express()
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', (req,res) =>{
+    console.log("here at home")
+    res.render('home')
 })
-.then(result => {
-    return Flavor.create({name:"apple"})
+app.post('/voting', async (req,res)=>{
+    console.log("from voting::", req.body)
+    const {flavor,username,usermail} = req.body
+    //check if user alredy exists
+    const dupUser = await User.findOne({ where: { email: usermail } });
+    if(!dupUser){
+        await User.create({
+            userName:username,
+            email:usermail,
+            foreignKey:flavor
+        })
+        res.redirect('/voted')
+    }else{
+        res.render('failed')
+    }
 })
-.then(res => console.log(res))
-.catch(err => {
-    console.log(err)
+app.get('/voted',(req,res)=>{
+    console.log(req.body)
+    res.render('voted',{userName : "hello"})
 })
+
+// sequelize.sync({alter:true})
+// .then(result =>{
+//     return Flavor.create({flavorName:"chocolate"})
+// })
+// .then(result =>{
+//     return Flavor.create({flavorName:"banana"})
+// })
+// .then(result =>{
+//     return Flavor.create({flavorName:"vanilla"})
+// })
+// .then(result =>{
+//     return Flavor.create({flavorName:"butterscotch"})
+// })
+// .then(result =>{
+//     return Flavor.create({flavorName:"mango"})
+// })
+
+app.listen(8080)
--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ class User extends Model{}
 
   User.init(
     {
-      username: {
+      userName: {
         type: DataTypes.TEXT,
         allowNull: false
       },
